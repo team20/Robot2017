eb5ce2b93d9aef471310d754cbbc1ae5bd5eda4c
Team 20 <team20programming@gmail.com>
2017-01-08 22:16:02 -0500

    Creation setup of the Potential Flywheel class.
    This Flywheel class can be used to test the prototypes of the flywheels as well as the final product if it is chosen as a method of scoring for the 2017 Robot

---
+++ FlyWheel.java
@@ -0,0 +1,25 @@
+ackage org.usfirst.frc.team20.robot;
+
+import edu.wpi.first.wpilibj.CANTalon;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.DriverStation;
+
+public class FlyWheel {
+	DriverStation d;
+	
+	public FlyWheel(){
+		
+	}
+  
+  public void flyWheelToSpeed(int speed){
+  
+  }
+	
+  public void stopFlyWheel(){
+  
+  }
+  
+  
+	
+	
+}
