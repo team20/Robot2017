64aab0a9f6e1aafef34a8cfeaa12148befe84698
saathviknarra <saathvik_narra@yahoo.com>
2017-01-21 11:33:31 -0500

    Added Parce and Temp Sensor

---
+++ TemperatureSensor.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+public class TemperatureSensor { 
+	ArrayList<Double> Temperature = new ArrayList<Double>();
+
+
+	public void ParseTemperature(String str){
+		String[] Data = str.split("\\*");
+		//ArrayList<Double>Temperature = new ArrayList<Double>();
+		Temperature.clear();
+		int all = 1;
+	
+		while (all < Data.length){
+			Temperature.add(Double.parseDouble(Data[all]));
+			all = all + 1;
+		}
+	}
+	public Double getTemperature(int index){
+		return (Double) Temperature.toArray()[index];		
+	}
+	public int getLength(){
+		return Temperature.size();
+	}
+}
+
